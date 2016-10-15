273271ff15cb455ccd6589855aaf68cdbe389149
Ken Morishita <mokemokechicken@gmail.com>
2016-10-15 10:28:48 +0900

    add access external api

--- routes/apis.js
+++ routes/apis.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const externalApiEndpoint = process.env.API_ENDPOINT || 'http://localhost:8989/status.txt';
+const externalApiResponse = process.env.API_RESPONSE || 'OK';
+const rp = require('request-promise');
 
 /* GET apis listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource!!');
 });
+
+router.all('*', function(req, res, next) {
+  rp({url: externalApiEndpoint, forever: true})
+    .then((body) => {
+      if (String(body).trim() == externalApiResponse) {
+        next();
+      } else {
+        res.send(500, `Status is not ${externalApiResponse}, but ${body}`);
+      }
+    })
+    .catch((err) => next(err))
+  ;
+});
+
+router.get("/status", (req, res) => res.send("OK\n"));
 
 router.use('/users', require('./users'));
 router.use('/articles', require('./articles'));
